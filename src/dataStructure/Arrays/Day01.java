77d0a5a3dcf7b04f78971d3c99670b0217f5fed5
kumar aditya <kumar.aditya@emc.com>
2020-06-04 08:03:19 +0530

    Initial commit

---
+++ src/dataStructure/Arrays/Day01.java
@@ -0,0 +1,15 @@
+package dataStructure.Arrays;
+
+public class Day01 {
+    /**
+     * We are going to start study datastructure Array for coding interview
+     * An array is a collection of items stored at contiguous memory locations.
+     * This makes it easier to calculate the position of each element by simply adding an offset to a base value,
+     * i.e., the memory location of the first element of the array (generally denoted by the name of the array).
+     *
+     * Advantage of using Arrays
+     * Arrays allow random access of elements. This makes accessing elements by position faster.
+     * Arrays have better cache locality that can make a pretty big difference in performance.
+     *
+     */
+}
