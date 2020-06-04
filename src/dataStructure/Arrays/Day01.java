73cf1efb1fadc57007d2d767d09841989e978186
kumar aditya <kumar.aditya@emc.com>
2020-06-04 10:01:15 +0530

    pushing project to local

--- src/dataStructure/Arrays/Day01.java
+++ src/dataStructure/Arrays/Day01.java
@@ -12,4 +12,20 @@ public class Day01 {
      * Arrays have better cache locality that can make a pretty big difference in performance.
      *
      */
+    //to print matrix to particular position(x,y)
+    public static void main(String[] args){
+        int [][] array_test = {{1,2,3},{4,5,6},{7,8,9}};
+        int start = 2;
+        int end = 2;
+        int sum;
+        for (int i=0;i<start;i++){
+            for(int j=0;j<array_test[i].length && !(i==start-1 && j==end) ;j++){
+                System.out.println("value of i :" + i + "value of j:" + j);
+                System.out.println(array_test[i][j]);
+
+            }
+        }
+    }
+
+
 }
