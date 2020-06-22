5958242ebc47bc49fb2fd68441b1c066fd817778
kumar aditya <kumar.aditya@emc.com>
2020-06-22 22:20:57 +0530

    pushing project to local

--- src/dataStructure/Arrays/LargestSumContiguousSubarray.java
+++ src/dataStructure/Arrays/LargestSumContiguousSubarray.java
@@ -2,11 +2,22 @@ package dataStructure.Arrays;
 /*
 problem statement: Write an efficient program to find the sum of contiguous subarray within a one-dimensional array of
 numbers which has the largest sum.
+Initialize:
+    max_so_far = 0
+    max_ending_here = 0
 
+Loop for each element of the array
+  (a) max_ending_here = max_ending_here + a[i]
+  (b) if(max_so_far < max_ending_here)
+            max_so_far = max_ending_here
+  (c) if(max_ending_here < 0)
+            max_ending_here = 0
+
+return max_so_far
  */
 public class LargestSumContiguousSubarray {
     public static void main(String[] args){
-        int[] input = {2,-4,7,-3,-1,8,4};
+        int[] input = {-2,-4,-7,-1,-9,-8,-14};
         int len  = input.length;
         int max = max_sum_subarray(input,len);
         System.out.println("maximum sum of subarray: "+ max);
